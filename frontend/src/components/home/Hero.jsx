a2d5394341389fd9aa4199de1d3cd51265bd89a2
Vytautas <vzvytautasz@gmail.com>
2024-10-08 14:36:51 +0300

    Created Hero jsx. Dont have styles so having red warnings.

---
+++ frontend/src/components/home/Hero.jsx
@@ -0,0 +1,23 @@
+import Button from "../common/Button";
+
+const Hero = () => {
+  return (
+    <div className={styles.hero}>
+      <h1 className={styles.title}>
+        Find Home <span className={styles.primary}>Service/Repair</span>
+        <br />
+        Near You
+      </h1>
+      <p className={styles.subtitle}>
+        Explore Best Home Service & Repair near You
+      </p>
+      <div className={styles.searchContainer}>
+        {/* <SearchInput /> */}
+        <Button rounded>
+          <div>{/* <CiSearch fontsize={24} /> */}</div>
+        </Button>
+      </div>
+    </div>
+  );
+};
+export default Hero;
