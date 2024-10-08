7e1da05b59473a3240f003ce885761d665e17c9c
Vytautas <vzvytautasz@gmail.com>
2024-10-08 15:03:12 +0300

    Created SearchInput with styles.

--- frontend/src/components/home/Hero.jsx
+++ frontend/src/components/home/Hero.jsx
@@ -1,3 +1,5 @@
+import { CiSearch } from "react-icons/ci";
+import SearchInput from "../common/SearchInput";
 import styles from "./Hero.module.scss";
 import Button from "../common/Button";
 
@@ -13,9 +15,11 @@ const Hero = () => {
         Explore Best Home Service & Repair near You
       </p>
       <div className={styles.searchContainer}>
-        {/* <SearchInput /> */}
+        <SearchInput />
         <Button rounded>
-          <div>{/* <CiSearch fontsize={24} /> */}</div>
+          <div>
+            <CiSearch fontsize={24} />
+          </div>
         </Button>
       </div>
     </div>
