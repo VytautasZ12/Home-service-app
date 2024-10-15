326bc6b706485bdeb558bd7331eda7c87dae7873
Vytautas <vzvytautasz@gmail.com>
2024-10-15 10:59:19 +0300

    Moved index.scss in to styles, fixed import in main.jsx file to import styles.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+import "./styles/index.scss";
 
 ReactDom.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
